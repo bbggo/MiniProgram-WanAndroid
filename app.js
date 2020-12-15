0ed9947cef6c6f25dcafbe36807a5e58f82aa811
wangyuebin <yuebin.wang-a1140@aqara.com>
2020-12-15 19:30:54 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//app.js
+App({
+
+  globalData: {
+    baseUrl: "https://www.wanandroid.com"
+  },
+
+  onLaunch: function () {
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let capsule = wx.getMenuButtonBoundingClientRect();
+        if (capsule) {
+          this.globalData.Custom = capsule;
+          this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
+        } else {
+          this.globalData.CustomBar = e.statusBarHeight + 50;
+        }
+      }
+    })
+  },
+
+})
---
+++ pages/index/index.js
@@ -0,0 +1,79 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var that = this;
+
+Page({
+  data: {
+    bannerList: [],
+    pageNumber: 0,
+    pageList: [],
+    isLoad: false,
+  },
+
+  onLoad: function () {
+    that = this;
+    this.getBanner();
+  },
+
+  onReachBottom: function () {
+    that = this;
+    that.setData({
+      isLoad: true,
+      pageNumber: that.data.pageNumber + 1
+    });
+    this.getPageList();
+  },
+
+  // cardSwiper
+  cardSwiper(e) {
+    this.setData({
+      cardCur: e.detail.current
+    })
+  },
+
+  getBanner: function () {
+    wx.request({
+      url: app.globalData.baseUrl + '/banner/json',
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log(res.data)
+        that.setData({
+          bannerList: res.data.data
+        })
+      }
+    })
+    this.getPageList();
+  },
+
+  getPageList: function () {
+    that = this;
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: app.globalData.baseUrl + '/article/list/' + that.data.pageNumber + '/json',
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log(res.data)
+        wx.hideNavigationBarLoading();
+        var list = res.data.data.datas;
+        var tempList = that.data.pageList;
+        var resultList = tempList.concat(list);
+        that.setData({
+          isLoad: false,
+          pageList: resultList
+        })
+      }
+    })
+  },
+  isCard(e) {
+    this.setData({
+      isCard: e.detail.value
+    })
+  },
+})
