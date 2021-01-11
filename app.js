b69869841884ba1e72a47ee1a80efd3f68ebe591
AnyWayGobin <395044952@qq.com>
2021-01-11 23:02:53 +0800

    add my

--- app.js
+++ app.js
@@ -68,7 +68,8 @@ App({
       name: 'black',
       color: '#333333'
     }
-  ]
+  ],
+  userInfo: null
   },
 
   onLaunch: function () {
@@ -81,6 +82,27 @@ App({
           this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
         } else {
           this.globalData.CustomBar = e.statusBarHeight + 50;
+        }
+      }
+    })
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
         }
       }
     })
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,18 +1,77 @@
 // pages/my/my.js
+var that = this;
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      console.log('userInfo = ', app.globalData.userInfo);
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      console.log('canIUse = ');
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        console.log(res);
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      console.log('getUserInfo = ');
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
 
+  login: function(e) {
+    this.getUserInfo(e);
+  },
+
+  getUserInfo(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  CopyLink(e) {
+    wx.setClipboardData({
+      data: e.currentTarget.dataset.link,
+      success(res) {
+        wx.showToast({
+          title: '已复制',
+          duration: 1000
+        })
+      }
+    })
   },
 
   /**
